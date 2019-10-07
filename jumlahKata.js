f44a2f66ea0ec8dce39e7d82240cf6cbb40836ce
Carl <carlh3h@yahoo.co.id>
2019-10-07 10:45:10 +0700

    fix jumlahKata.js

--- jumlahKata.js
+++ jumlahKata.js
@@ -1,6 +1,17 @@
 function hitungJumlahKata(kalimat) {
   // you can only write your code here!
-  return kalimat.split(' ').length
+  // return kalimat.split(' ').length
+  var temp = ''
+  var res = [];
+  for (var i = 0; i < kalimat.length; i += 1){
+    temp += kalimat[i]
+    if (kalimat[i+1] == ' ' || i == kalimat.length-1){
+      res.push(temp)
+      temp = ''
+      i += 1
+    }
+  }
+  return(res.length)
 }
 
 // TEST CASES
